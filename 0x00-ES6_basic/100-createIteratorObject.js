0397c791a2fb5b8870d002b2c906dc952b59440e
bustlezach <bustlezach001@gmail.com>
2023-09-25 21:23:37 -0700

    task

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,12 @@
 export default function createIteratorObject(report) {
-    const workers = [];
-    for (const [department, employees] of Object.entries(report.allEmployees)) {
-      for (const employe of employees) {
-        workers.push(employe);
-      }
+  const workers = [];
+  /* eslint-disable no-unused-vars */
+  for (const [department, employees] of Object.entries(report.allEmployees)) {
+    for (const employe of employees) {
+      workers.push(employe);
     }
-  
-    return workers;
- }
+  }
+  /* eslint-enable no-unused-vars */
+
+  return workers;
+}
