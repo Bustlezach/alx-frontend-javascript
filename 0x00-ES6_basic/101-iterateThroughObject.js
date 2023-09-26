a19a2cb9624d442ad9dbffb9d6b69aac3045d8ac
bustlezach <bustlezach001@gmail.com>
2023-09-25 21:25:20 -0700

    task

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,13 @@
-export default function createEmployeesObject(departmentName, employees) {
-    return { [departmentName]: employees };
+export default function iterateThroughObject(reportWithIterator) {
+  let employees = '';
+
+  for (const [i, value] of reportWithIterator.entries()) {
+    if (i !== reportWithIterator.length - 1) {
+      employees += `${value} | `;
+    } else {
+      employees += value;
+    }
+  }
+
+  return employees;
 }
