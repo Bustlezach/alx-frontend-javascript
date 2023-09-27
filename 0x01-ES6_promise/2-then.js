b2593bf3d2f6a873279fb537e6256f4841a522ef
bustlezach <bustlezach001@gmail.com>
2023-09-27 09:42:14 -0700

    task

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,11 @@
 function handleResponseFromAPI(promise) {
-  const myPromise = promise
-    .then(() => ({
-      status: 200,
-      body: 'Success',
-    }))
-    .catch(() => Error())
+  return promise.then(() => ({
+    status: 200,
+    body: 'success',
+  })).catch(() => Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
-
-  return myPromise;
 }
-
 
 export default handleResponseFromAPI;
