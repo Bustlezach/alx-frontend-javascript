a88a28a51eded4136121122a41e71a2e0cd85b04
bustlezach <bustlezach001@gmail.com>
2023-09-26 18:27:07 -0700

    task

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+/* global Promise console */
+export default function getResponseFromAPI() {
+  const myPromise = new Promise((resolve, reject) => {
+    // Create a condition
+    const student = true;
+    if (student) {
+      resolve('I am an ALX SE student.');
+    } else {
+      // Failed
+      reject(new Error('Sorry, you are not an ALX student.'));
+    }
+  });
+  return myPromise;
+}
+
+getResponseFromAPI()
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((message) => {
+    console.error(message);
+  });
+
