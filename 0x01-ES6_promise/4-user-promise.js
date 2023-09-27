aa0d11a7a102ff1b17e1d6c7bc9a79da3ef016a9
bustlezach <bustlezach001@gmail.com>
2023-09-27 15:42:37 -0700

    task

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,5 @@
 function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName,
-    lastName
-  });
+  return Promise.resolve({ firstName, lastName });
 }
 
 export default signUpUser;
