895c3ea48f05c544877de76b98faa5d5f5ca15db
bustlezach <bustlezach001@gmail.com>
2023-09-27 08:25:12 -0700

    task

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,15 +1,12 @@
-function getFullResponseFromAPI(success) {
-  const myPromise = new Promise((resolve, reject) => {
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
-        body: 'Success'
+        body: 'Success',
       });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
-  return myPromise;
 }
-
-export default getFullResponseFromAPI;
