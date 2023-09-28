300113efe7ef1257bd8723c570fc7ef6bf31d15b
bustlezach <bustlezach001@gmail.com>
2023-09-28 12:27:10 -0700

    task

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,11 +1,8 @@
 // eslint-disable-next-line no-unused-vars
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 class Pricing {
   constructor(amount, currency) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number.');
-    }
     this._amount = amount;
     this._currency = currency;
   }
@@ -14,16 +11,12 @@ class Pricing {
     return this._amount;
   }
 
-  set amount(value) {
-    if (typeof value === 'number') {
-      this._amount = value;
-    } else {
-      throw new TypeError('Amount must be a number.');
-    }
-  }
-
   get currency() {
     return this._currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
   }
 
   set currency(value) {
@@ -31,13 +24,13 @@ class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
-    const calc = (amount * conversionRate);
-    return calc;
+    return (amount * conversionRate);
   }
 }
+
 
 export default Pricing;
