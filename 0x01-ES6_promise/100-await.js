dfd5d8db775091e90b66badd55069728422a51de
bustlezach <bustlezach001@gmail.com>
2023-09-27 18:45:59 -0700

    task

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,15 +4,9 @@ async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    return {
-      photo,
-      user,
-    };
+    return {photo, user};
   } catch (error) {
-    return {
-      photo: null,
-      user: null,
-    };
+    return {photo: null, user: null};
   }
 }
 
