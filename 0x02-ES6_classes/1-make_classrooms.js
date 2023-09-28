1ba9776f1cbd13d92068d66eaff9503c1f08a8cf
bustlezach <bustlezach001@gmail.com>
2023-09-28 06:21:29 -0700

    task

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,12 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
-  ];
-  return rooms;
+  const classroomList = [];
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  classroomList.push(room1, room2, room3);
+  return classroomList;
 }
 
 export default initializeRooms;
