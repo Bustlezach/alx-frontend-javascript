22834de94ab36c9256b3437488715954b40fbafa
bustlezach <bustlezach001@gmail.com>
2023-09-29 08:04:31 -0700

    task

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,24 +1,13 @@
 class HolbertonClass {
   constructor(size, location) {
-    if (typeof size !== 'number') {
-      throw new TypeError('Size must be a number.');
-    }
     this._size = size;
-
-    if (typeof location !== 'string') {
-      throw new TypeError('Location must be a string.');
-    }
     this._location = location;
   }
-  
+
   [Symbol.toPrimitive](hint) {
-    if (hint === 'number') {
-      return this._size;
-    }
-    if (hint === 'string') {
-      return this._location;
-    }
-    return 42;
+    if (hint === 'string') return this._location;
+    if (hint === 'number') return this._size;
+    return 'default';
   }
 }
 
