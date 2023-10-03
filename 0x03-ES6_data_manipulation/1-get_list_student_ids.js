d801e34cac943bb63bea600610792c60db0af535
bustlezach <bustlezach001@gmail.com>
2023-10-02 18:49:27 -0700

    task

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,10 +2,6 @@ function getListStudentIds(studentObjects) {
   if (!Array.isArray(studentObjects)) {
     return [];
   }
-  
-  const studentIds = studentObjects.map((student) => student.id);
-  
-  return studentIds;
+  return studentObjects.map((student) => student.id);
 }
-
 export default getListStudentIds;
